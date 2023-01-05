773e9126753898b7088fba66617a1f62024c756c
ngthung0909 <nthung0909>
2023-01-05 19:57:53 +0700

    hehe

--- src/index.js
+++ src/index.js
@@ -11,25 +11,26 @@ import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <CookiesProvider>
-      <ToastContainer
-      position="top-center"
-      autoClose={1000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-      <BrowserRouter>
-        <Provider store={configureStore()}>
-          <PageRouters />
-        </Provider>
-      </BrowserRouter>
-    </CookiesProvider>,
+  <>hello word</>,
+    // <CookiesProvider>
+    //   <ToastContainer
+    //   position="top-center"
+    //   autoClose={1000}
+    //   hideProgressBar={false}
+    //   newestOnTop={false}
+    //   closeOnClick
+    //   rtl={false}
+    //   pauseOnFocusLoss
+    //   draggable
+    //   pauseOnHover
+    //   theme="light"
+    // />
+    //   <BrowserRouter>
+    //     <Provider store={configureStore()}>
+    //       <PageRouters />
+    //     </Provider>
+    //   </BrowserRouter>
+    // </CookiesProvider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
